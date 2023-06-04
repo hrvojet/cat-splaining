55286a5ad2752bdfc29d556060907c530afd307f
Hrvoje <hrvojetrcak@gmail.com>
2023-06-04 03:23:12 +0200

    Add start and build yarn scripts

--- scripts/juice.js
+++ scripts/juice.js
@@ -1,30 +1,29 @@
 let juice = require('juice');
-let fs = require('fs');
+const fs = require("fs");
 
 const htmlPath = '../public/index.html';
-const cssPath = '../public/style.css';
-
-/*juice.juiceFile(
-    htmlPath,
-    {
-        extraCss: cssPath
-    },
-    function(err, html){
-        console.log(err);
-        console.log(html);
-});*/
 
 try {
-    const htmlStr = fs.readFileSync(htmlPath);
-    const cssStr = fs.readFileSync(cssPath);
-    const ht = juice.inlineContent(htmlStr.toString(), cssStr.toString(), {applyStyleTags: false});
-    console.log(ht);
-    fs.writeFileSync('../index.html', ht, function (err) {
+    process.chdir('./scripts');
+    console.log('New directory: ' + process.cwd());
+}
+catch (err) {
+    console.error('chdir: ' + err);
+}
+
+juice.juiceFile(
+    htmlPath,
+    {},
+    function(err, html){
         if (err) {
-            console.log('Error writing to file!');
-            console.error(err);
+            console.log(err);
+        } else {
+            fs.writeFileSync('../index.html', html, function (err) {
+                if (err) {
+                    console.log('Error writing to file!');
+                    console.error(err);
+                }
+            });
         }
     });
-} catch (err) {
-    console.error(err);
-}
+
